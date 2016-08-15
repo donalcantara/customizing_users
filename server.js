bedc190da27eba0c9ab619f8bebead4f34fd0563
Don Alcantara <don.alcantara@yahoo.com>
2016-08-15 14:22:45 -0700

    minor tweaks

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-// Require the Express Module
+
 var express = require("express");
-// Create an Express App
+
 var app = express();
 var mongoose = require('mongoose');
-// This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
 
 var express = require('express');
 var app = express();
@@ -11,21 +10,18 @@ var app = express();
 var path = require("path");
 // Require body-parser (to receive post data from clients)
 var bodyParser = require("body-parser");
-// Integrate body-parser with our App
-app.use(bodyParser.urlencoded());
-// Setting our Static Folder Directory
-app.use(express.static(path.join(__dirname, "./static")));
-// Setting our Views Folder Directory
 
-// Routes
-// Root Request
+app.use(bodyParser.urlencoded());
+
+app.use(express.static(path.join(__dirname, "./static")));
+
 app.get('/', function(req, res) {
     res.render('index');
 });
 
 
 
-// Setting our Server to Listen on Port: 8000
+// Setting Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
 })
